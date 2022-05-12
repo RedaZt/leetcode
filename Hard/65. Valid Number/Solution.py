85a4de3ad4094b3de1b12cd1bbe024652019b67e
RedaZt <redazitouni245@gmail.com>
2022-05-12 07:54:04 +0100

    Valid Number Solution

--- Hard/65. Valid Number/Solution.py
+++ Hard/65. Valid Number/Solution.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def isNumber(self, s):
         try :
-            if not any(x.isnumeric()for x in s): return False
+            if not any(x.isnumeric()for x in s): 
+                return False
             a = float(s)
             return True
         except:
