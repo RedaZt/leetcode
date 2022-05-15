43eccc259386a3345c3144f04563161ddea10100
RedaZt <redazitouni245@gmail.com>
2022-05-15 01:36:39 +0100

    Two Sum Solution

--- Easy/1. Two Sum/Solution.py
+++ Easy/1. Two Sum/Solution.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i,v in enumerate(nums):
+        for i, v in enumerate(nums):
             b = target - v
             if b in nums and (v!=b or nums.count(v)>1):
                 return [i, i+1+nums[i+1:].index(b)]
