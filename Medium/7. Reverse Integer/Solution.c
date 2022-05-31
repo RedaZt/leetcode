c86b59c9761c52c059f7d98bdb006b1cdb7fea0b
RedaZt <redazitouni245@gmail.com>
2022-05-31 03:09:09 +0100

    Reverse Integer Solution

---
+++ Medium/7. Reverse Integer/Solution.c
@@ -0,0 +1,20 @@
+int reverse(long long int x){
+    long long int r = 0;
+    int s = 1;
+    
+    if (x<0) {
+        s = -1;
+        x = x * -1;
+    }
+    
+    while (x) {
+        r *= 10;
+        r += x%10;
+        x /= 10;
+    }
+    
+    if (r > pow(2, 31))
+        return 0;
+    
+    return r * s;
+}
