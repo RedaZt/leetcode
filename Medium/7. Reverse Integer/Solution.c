d76b89d3623ce709c3beb8756e1fc7064ed1c086
RedaZt <redazitouni245@gmail.com>
2022-06-01 19:45:15 +0100

    Reverse Integer Solution

--- Medium/7. Reverse Integer/Solution.c
+++ Medium/7. Reverse Integer/Solution.c
@@ -2,7 +2,7 @@ int reverse(long long int x){
     long long int r = 0;
     int s = 1;
     
-    if (x<0) {
+    if (x < 0) {
         s = -1;
         x = x * -1;
     }
--- Medium/7. Reverse Integer/Solution.js
+++ Medium/7. Reverse Integer/Solution.js
@@ -1,6 +1,6 @@
 var reverse = function (x) {
     s = +[...Math.abs(x).toString()].reverse().join('');
-    if (s > Math.pow(2, 31)) 
-        s=0;
+    if (s > Math.pow(2, 31))
+        s = 0;
     return x > 0 ? s : -s;
 };
