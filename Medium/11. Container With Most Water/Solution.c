3b0a3de509133916930a8e9fe793bf6486c3324f
Reda <redazitouni245@gmail.com>
2022-11-01 05:48:47 +0100

    Container With Most Water Solution

--- Medium/11. Container With Most Water/Solution.c
+++ Medium/11. Container With Most Water/Solution.c
@@ -1,15 +1,17 @@
 int maxArea(int* height, int heightSize){
-    int m=0, p;
-    for (int i=0; i<heightSize; i++){
-        if (height[i]*(heightSize-i) > m){
-            for (int j=i+1; j<heightSize; j++){
-                p = height[i];
-                if (height[j] < height[i])
-                    p = height[j];
-                if (p * (j-i) > m)
-                    m = p * (j-i);
-            }
-        }   
+    int m = 0, start = 0, end = heightSize - 1, minHeight, res;
+    while (start != end){
+        minHeight = height[start];
+        if (height[end] < minHeight)
+            minHeight = height[end];
+        res = minHeight * (end - start);
+        if (res > m)
+            m = res;
+        if (height[start] < height[end]) {
+            start++;
+        } else {
+            end--;
+        } 
     }
     return m;
 }
